ad8bc9e950843a0a65f13e75939944ebb37a3fa4
BRZeph <148656729+BRZeph@users.noreply.github.com>
2025-04-29 12:03:26 -0300

    init

---
+++ src/main/java/brzeph/spring/java_spring_demo_erudio/JavaSpringDemoErudioApplication.java
@@ -0,0 +1,13 @@
+package brzeph.spring.java_spring_demo_erudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringDemoErudioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringDemoErudioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/brzeph/spring/java_spring_demo_erudio/JavaSpringDemoErudioApplicationTests.java
@@ -0,0 +1,13 @@
+package brzeph.spring.java_spring_demo_erudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringDemoErudioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
